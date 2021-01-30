098dc2b1707c6b66ffc9d5781e1b4f00f878563a
Josh Frank <eurosewusa@gmail.com>
2021-01-29 21:22:28 -0500

    commit tree caa9ba51facfc6dda7d9c4bb6c08a14e5ffcc233

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards( names, message ) {
+    let result = [];
+    for ( let i = 0; i < names.length; i++ ) {
+        result.push( `Thank you, ${ names[ i ] }, for the wonderful ${ message } gift!` );
+    }
+    return result;
+}
+
+function countDown( number ) {
+    while ( number > -1 ) {
+        console.log( number );
+        number--;
+    }
+}
